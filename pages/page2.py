0bad329a3c34f74ad5cad90bf933253d98c6a238
VoidPHF <PHenrique.F.Lima@hotmail.com>
2024-12-17 18:56:50 -0300

    Multipages

---
+++ pages/page2.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Página 2")
+st.write("Este é o conteúdo da Página 2")
---
+++ pages/page3.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Página 3")
+st.write("Este é o conteúdo da Página 3")
