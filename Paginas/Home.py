7c1a7a57f9a0dfc78948d42db80b121543c9fc1a
JEFERSON DE LIMA MENDES <148601057+JefersonUFRPEBSI@users.noreply.github.com>
2025-03-03 15:35:48 -0300

    Delete Paginas/Home.py

--- Paginas/Home.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def main():
-    st.title("Bem-vindo à Página Inicial!")
-    st.write("Essa é a página principal da aplicação.")
-    st.write("Use o menu lateral para navegar pelas páginas.")
